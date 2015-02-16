14d2429c1cac48f296b8a5f34a1f5b76bce9bb7f
adrenalbooster <kontakt@internetowakuznia.pl>
2015-02-16 23:01:00 +0100

    not-fixed Products model yet, goodnight commit.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :description, :title, :price
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
+  def self.average_rating
+    product.ratings.average(:rating)
+  end
 end

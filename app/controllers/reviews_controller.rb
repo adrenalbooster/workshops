29ce2ad0d187fa519b081f66f300b996a4bf51a2
kkulikowski <kontakt@internetowakuznia.pl>
2015-03-06 22:25:19 +0100

    Add uniqueness to names, fixed multiple reviews of one product by the same user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,4 @@
 class ReviewsController < ApplicationController
-
   expose(:review)
   expose(:product)
 
@@ -9,11 +8,16 @@ class ReviewsController < ApplicationController
   def create
     self.review = Review.new(review_params)
     review.user = current_user
+    unless Review.exists?( {product_id: product, user_id: current_user})
     if review.save
       product.reviews << review
       redirect_to category_product_url(product.category, product), notice: 'Review was successfully created.'
     else
       render action: 'new'
+    end
+    else
+      flash[:error] = "You gave us your review already."
+      redirect_to category_product_url(product.category, product)
     end
   end
 
